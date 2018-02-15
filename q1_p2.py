5b232a91c617e1965f4fa2b64c58de5061454853
Fanny <fannydai@gmail.com>
2018-02-15 15:48:33 -0500

    Started working on Q1P2

---
+++ q1_p2.py
@@ -0,0 +1,22 @@
+# Fanng Dai
+# Stony Brook University
+# CSE337
+# Due March 1st
+# Question 1 part 2 of Homework 1
+
+# Part 2. Go over the prices file, and calculate the max, min, mean and the standard
+# deviation for the prices. Find the corresponding dates for max, min and mean, and print
+# them to screen. [13 pts]
+
+max = 0
+min = 0
+Avg = 0
+sd # Standard Deviation
+sum
+totalAmt
+
+print("The max is " + str(max) + "\n")
+print("The date associated with that is ")
+print("The min is " + str(min) + "\n")
+print("The date associated with that is ")
+print("The mean is " + str(avg) + "\n")
