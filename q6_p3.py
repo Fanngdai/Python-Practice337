b42e691600bc8fd4f2b3f399ac9fc5c790736d57
Fanny <fannydai@gmail.com>
2018-02-16 02:59:28 -0500

    Q6_P3 made interleave function accept only 2 params

--- q6_p3.py
+++ q6_p3.py
@@ -10,24 +10,25 @@
 # individual lists are preserved. For example, for inputs [1,2] and [3,4], your program
 # should result in [1,2,3,4],[1,3,4,2],[1,3,2,4],[3,4,1,2],[3,1,2,4],[3,1,4,2]. [3 pts].
 
-def interleave(lst1, lst2, res, i, j, lis):
+def interleave(lst1, lst2):
+    list = []
+    helper(lst1, lst2, [], 0, 0, list)
+    print(list)
+
+def helper(lst1, lst2, res, i, j, lis):
     if i == len(lst1) and j == len(lst2):
         list = res[:]
         lis.append(list)
         return
     if i < len(lst1):
         res.append(lst1[i])
-        interleave(lst1, lst2, res, i + 1, j, lis)
+        helper(lst1, lst2, res, i + 1, j, lis)
         res.remove(lst1[i])
     if j < len(lst2):
         res.append(lst2[j])
-        interleave(lst1, lst2, res, i, j + 1, lis)
+        helper(lst1, lst2, res, i, j + 1, lis)
         res.remove(lst2[j])
 
-
-res = []
-lis = []
 lst1 = [1,2]
 lst2 = [3,4]
-interleave(lst1, lst2, res, 0, 0, lis)
-print(lis)
+interleave(lst1, lst2)
