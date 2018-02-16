655915ca5d8313ed65749a74db5d3aee08b01724
Fanny <fannydai@gmail.com>
2018-02-16 00:21:46 -0500

    Gonna go shower Working on q6p3

--- q6_p3.py
+++ q6_p3.py
@@ -10,3 +10,12 @@
 # individual lists are preserved. For example, for inputs [1,2] and [3,4], your program
 # should result in [1,2,3,4],[1,3,4,2],[1,3,2,4],[3,4,1,2],[3,1,2,4],[3,1,4,2]. [3 pts].
 
+def interleave(lst1, lst2):
+    if len(lst1)==0:
+        return lst2
+    elif len(lst2)==0:
+        return lst1
+    else:
+        return [lst1[0], lst2[0]] + interleave(lst1[1:], lst2[1:])
+
+interleave([1,2],[3,4])
