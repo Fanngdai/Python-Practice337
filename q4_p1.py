a6bef2911570e17329ae891313951531f58fd481
Fanny <fannydai@gmail.com>
2018-02-19 18:50:26 -0500

    Q4_P1 done

--- q4_p1.py
+++ q4_p1.py
@@ -11,9 +11,28 @@
 # returns a dictionary where keys are words, and the values are the number of occurrence
 # of these words.
 # You can test your program on this file after downloading it:
-# http://www.gutenberg.org/files/100/ 100-0.txt
+# http://www.gutenberg.org/files/100/100-0.txt
 
 # Part 1. Write a Python program that parses the given file path and returns a
 # dictionary where keys are the words in the text, and the values are the word counts. A
 # word is defined by the space characters around it. You should ignore the empty lines
 # [10 pts].
+
+def wordAnalytic():
+    word = {}
+    input = raw_input("Enter File Name: ");
+
+    try:
+        file = open(input, "r");
+    except IOError:
+        print "File not found"
+    else:
+        line = file.readline()
+    
+        while line:
+            for ele in line.split(" "):
+                ele = ele.strip()
+                word[ele] = word.get(ele,0) + 1
+            line = file.readline()
+        file.close()
+    return word
